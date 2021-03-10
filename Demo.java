36a57e93d29aaad0d547afc786ef585522707380
MekhiW34 <washington.mekhi@yahoo.com>
2021-03-10 13:15:31 -0500

    I just add my demo1 file

---
+++ Demo.java
@@ -0,0 +1,8 @@
+package Test;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("I got my first bike in the hood from crackhead billy for $25");
+    }
+}
+//C:\Users\washi\IdeaProjects\testhomework\src\main\java\Test\Demo.java
